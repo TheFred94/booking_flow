dcb3380e49dc104eb2cbe794e36803c55b15ac71
Jachob Westh <jachobwesth@gmail.com>
2023-05-26 13:54:49 +0200

    global css og styling

--- src/components/ChooseAmount.js
+++ src/components/ChooseAmount.js
@@ -3,9 +3,9 @@ import { TicketAmountPicker } from "./TicketAmountPicker";
 export default function ChooseAmount() {
   return (
     <>
-      <div className="mx-3 rounded-2 mb-8">
+      <div className="rounded-2 mx-3 mb-8">
         <div className="grid place-content-center bg-gradient-to-b from-color-opacity-20 to-color-opacity-10 ">
-          <h2 className="text-center m-8 serif">Amount</h2>
+          <h2 className="serif m-8 text-center uppercase">Amount</h2>
           <TicketAmountPicker />
         </div>
       </div>
--- src/components/ChooseArea.js
+++ src/components/ChooseArea.js
@@ -8,15 +8,15 @@ export function ChooseArea() {
   // fetches the different areas from api, and sets state of area to the fechted data
   useEffect(() => {
     fetch(`http://localhost:8080/available-spots`)
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         setAreas(data);
       });
   }, []);
 
   return (
     <>
-      <h2 className="text-center mt-16 mb-5">Choose area</h2>
+      <h2 className="mb-5 mt-16 text-center uppercase">Choose area</h2>
 
       {/* if areas is empty, then don't show anything, else show AreaList component.  */}
       {areas === "" ? <p></p> : <AreaList areas={areas} />}
